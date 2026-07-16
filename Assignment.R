775ae89a7fd6c04190b4888ec78f3216358036e4
Favour590 <okosunf34@gmail.com>
2026-07-16 12:29:11 +0100

    Add files via upload
    Added an external file

---
+++ Assignment.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+Invoice<-read.csv(file.choose())
+View(Invoice)
+  filter function
+Branches<-filter(Invoice,Branch=="Alex")
+View(Branches)
+   mutate function
+rates<-mutate(Invoice,Rate=Rating*10)
+view(rates)
+      ggplot function
+ggplot(Invoice,aes(y=cogs))+geom_bar()
+ggplot(Invoce,aes(x=gross.income))+geom_freqpoly()
+ggplot(Invoice,aes(y=cogs))+geom_histogram()
+ggplot(Invoice,aes(x=gross.income,y=cogs))+geom_boxplot()
